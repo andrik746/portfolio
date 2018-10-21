1577d6b08947fa90fa8ce6d6a42eef8416696e1f
Andrian Krysak <andriankrysak@gmail.com>
2018-10-21 12:01:38 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,90 @@
+
+(function($) {
+
+	skel.breakpoints({
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)',
+		xxsmall: '(max-width: 360px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body');
+
+		// Disable animations/transitions until the page has loaded.
+			$body.addClass('is-loading');
+
+			$window.on('load', function() {
+				window.setTimeout(function() {
+					$body.removeClass('is-loading');
+				}, 100);
+			});
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on medium.
+			skel.on('+medium -medium', function() {
+				$.prioritize(
+					'.important\\28 medium\\29',
+					skel.breakpoint('medium').active
+				);
+			});
+
+		// Items.
+			$('.item').each(function() {
+
+				var $this = $(this),
+					$header = $this.find('header'),
+					$a = $header.find('a'),
+					$img = $header.find('img');
+
+				// Set background.
+					$a.css('background-image', 'url(' + $img.attr('src') + ')');
+
+				// Remove original image.
+					$img.remove();
+
+			});
+
+	});
+
+})(jQuery);
+
+
+
+
+////////////////////////SCROLL ANIMATION////////////////////////
+
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+      (element_top_position <= window_bottom_position)) {
+      $element.addClass('animated');
+    } else {
+      $element.removeClass('animated');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
+////////////////////////SCROLL ANIMATION END ///////////////////////
